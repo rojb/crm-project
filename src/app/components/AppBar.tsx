6dfa5fbac977f43ceaee5ad4656c7527e52550a9
Ruben Justiniano <ruben.justiniano96@gmail.com>
2025-05-26 01:10:44 -0400

    feat(design-system): add initial design system with custom colors, typography and layout components

--- src/app/components/AppBar.tsx
+++ src/app/components/AppBar.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-
 export const AppBar = () => {
     return (
-        <div className='flex w-full h-16'>
+        <div className='flex w-full justify-between shadow-md px-3 text-center'>
             <div className="">
-                <h1>LeadTrack</h1>
+                <h1><span className="text-main font-semibold text-4xl">Lead</span><span className="text-secondary font-semibold text-4xl">Track</span></h1>
             </div>
-            <div className="flex">
-                <div className="">Buscar</div>
-                <div className="">Filtros</div>
+            <div className="flex items-center space-x-4">
+                <div className="flex items-center bg-background rounded-2xl w-96 px-4 shadow-sm">
+                    <img src="/search.svg" alt="Buscar" className="w-5 h-5" />
+                    <input
+                        type="text"
+                        placeholder="Buscar prospectos"
+                        className="w-full bg-transparent text-black focus:outline-none"
+                    />
+                </div>
+                {/* <div className="cursor-pointer text-main font-medium">Filtros</div> */}
             </div>
         </div>
     )
