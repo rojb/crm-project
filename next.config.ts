826ff6363d60eb54fd86080d50ce2daea1dfc70b
Ruben Justiniano <ruben.justiniano96@gmail.com>
2025-05-28 15:22:44 -0400

    feat(ui): improve dashboard layout

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
       },
     },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    })
+    return config;
   },
 };
 
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,9 +4,9 @@ import { AppBar, Sidebar } from '../components';
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
 
     return (
-        <div className="bg-[#F7F9FB] w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white flex flex-col">
+        <div className="bg-[#F7F9FB] w-full h-screen antialiased text-slate-300 selection:bg-main-600 selection:text-white grid grid-rows-[auto_1fr]">
             <AppBar />
-            <div className="flex flex-1">
+            <div className="grid grid-cols-[auto_1fr] overflow-hidden">
                 <Sidebar />
                 <div className="p-4 w-full text-slate-900 overflow-y-auto">
                     {children}
